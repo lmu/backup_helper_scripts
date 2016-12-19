846f5b7c839ad379b8fac631569d695435c5c4f5
Alexander Loechel <Alexander.Loechel@lmu.de>
2016-12-19 18:17:32 +0100

    target is directory

--- move-backups.sh
+++ move-backups.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Copy Piwik Backups"
-scp ansible@piwiktest1.verwaltung.uni-muenchen.de:/data/backup/* /backup/piwik/
+scp ansible@piwiktest1.verwaltung.uni-muenchen.de:/data/backup/* /backup/piwik
 
 echo "Copy Redmine Backups"
-scp ansible@redmine1.verwaltung.uni-muenchen.de:/data/redmine.buildout/backup/* /backup/redmine/
+scp ansible@redmine1.verwaltung.uni-muenchen.de:/data/redmine.buildout/backup/* /backup/redmine
 
 echo "Copy Plone Backups"
-scp ansible@cms-live-appdb1.verwaltung.uni-muenchen.de:/data/backup/* /backup/plone/
+scp ansible@cms-live-appdb1.verwaltung.uni-muenchen.de:/data/backup/* /backup/plone
 
 echo "Cleanup Backups (Delete old unnecessary Backups)"
 /home/fachadmin/backup_helper_scripts/cleanup-backups.py
