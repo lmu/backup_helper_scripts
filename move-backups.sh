1ccd3e7dc7f187ce6f8d65e4fc254237b969e417
Alexander Loechel <Alexander.Loechel@lmu.de>
2016-12-19 18:36:48 +0100

    preserve timestamp

--- move-backups.sh
+++ move-backups.sh
@@ -3,13 +3,13 @@
 sudo chown -R fachadmin:ansible /backup/*
 
 echo "Copy Piwik Backups"
-scp ansible@piwiktest1.verwaltung.uni-muenchen.de:/data/backup/* /backup/piwik
+scp -p ansible@piwiktest1.verwaltung.uni-muenchen.de:/data/backup/* /backup/piwik
 
 echo "Copy Redmine Backups"
-scp ansible@redmine1.verwaltung.uni-muenchen.de:/data/redmine.buildout/backup/* /backup/redmine
+scp -p ansible@redmine1.verwaltung.uni-muenchen.de:/data/redmine.buildout/backup/* /backup/redmine
 
 echo "Copy Plone Backups"
-scp ansible@cms-live-appdb1.verwaltung.uni-muenchen.de:/data/backup/* /backup/plone
+scp -p ansible@cms-live-appdb1.verwaltung.uni-muenchen.de:/data/backup/* /backup/plone
 
 echo "Cleanup Backups (Delete old unnecessary Backups)"
 /home/fachadmin/backup_helper_scripts/cleanup-backups.py
