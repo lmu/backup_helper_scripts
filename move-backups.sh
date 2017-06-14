b5a05db3ea0018098615b674861d851764caa142
Alexander Loechel <Alexander.Loechel@lmu.de>
2017-06-14 11:05:37 +0200

    move for root fit gemacht

--- move-backups.sh
+++ move-backups.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-sudo chown -R fachadmin:ansible /backup/*
+sudo chown -R root:ansible /backup/*
 
 echo "Copy Piwik Backups"
-scp -p ansible@piwiktest1.verwaltung.uni-muenchen.de:/data/backup/* /backup/piwik
+scp -i /home/fachadmin/.ssh/id_rsa -p ansible@piwiktest1.verwaltung.uni-muenchen.de:/data/backup/* /backup/piwik
 
 echo "Copy Redmine Backups"
-scp -p ansible@redmine1.verwaltung.uni-muenchen.de:/data/redmine.buildout/backup/* /backup/redmine
+scp -i /home/fachadmin/.ssh/id_rsa -p ansible@redmine1.verwaltung.uni-muenchen.de:/data/redmine.buildout/backup/* /backup/redmine
 
 echo "Copy Plone Backups"
-sudo rsync --rsync-path="sudo rsync" -rlpt --delete -e ssh ansible@cms-live-appdb1.verwaltung.uni-muenchen.de:/data/backups/ /backup/plone
+rsync --rsync-path="sudo rsync" -rlpt --delete -e "ssh -i /home/fachadmin/.ssh/id_rsa" ansible@cms-live-appdb1.verwaltung.uni-muenchen.de:/data/backups/ /backup/plone
 
 echo "Cleanup Backups (Delete old unnecessary Backups)"
 /home/fachadmin/backup_helper_scripts/cleanup-backups.py
